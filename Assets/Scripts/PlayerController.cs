79048e9b2c726d8b85bacf5851c31b98fd2292f6
Jan <57482512+jacka04@users.noreply.github.com>
2025-10-30 22:24:45 +0100

    Ground test for jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,36 +2,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float speed = 5f;
+    public float speed = 8f;
     public float jumpForce = 7f;
+
     private Rigidbody2D rb;
     private bool isGrounded;
 
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Movimiento horizontal
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
 
+        // Comprobamos si está en el suelo
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
+        // Saltar
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-
     }
+
     void OnDrawGizmosSelected()
     {
         if (groundCheck != null)
