d38981b8ff2a046431936e4e1a102f6ce4c34a1c
Jan <57482512+jacka04@users.noreply.github.com>
2026-03-18 09:35:12 +0100

    Menú casi fet vamos!!!

--- Assets/Scripts/MovingElevator.cs
+++ Assets/Scripts/MovingElevator.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using System.Collections;
 
 public class MovingElevator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float distanciaASubir = 5f;
+    public float velocidad = 2f;
+    public float tiempoEsperaArriba = 2f;
+
+    private bool moviéndose = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player") && !moviéndose)
+        {
+            StartCoroutine(CicloPlataforma());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator CicloPlataforma()
     {
-        
+        moviéndose = true;
+
+        Vector2 posicionInicial = transform.position;
+        Vector2 posicionObjetivo = posicionInicial + Vector2.up * distanciaASubir;
+
+        // SUBIDA
+        float progreso = 0;
+        while (progreso < 1)
+        {
+            progreso += Time.deltaTime * (velocidad / distanciaASubir);
+            transform.position = Vector2.Lerp(posicionInicial, posicionObjetivo, progreso);
+            yield return null;
+        }
+        transform.position = posicionObjetivo;
+
+        // ESPERA
+        yield return new WaitForSeconds(tiempoEsperaArriba);
+
+        // BAJADA
+        progreso = 0;
+        while (progreso < 1)
+        {
+            progreso += Time.deltaTime * (velocidad / distanciaASubir);
+            transform.position = Vector2.Lerp(posicionObjetivo, posicionInicial, progreso);
+            yield return null;
+        }
+        transform.position = posicionInicial;
+
+        moviéndose = false;
     }
 }
