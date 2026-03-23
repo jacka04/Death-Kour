aa62f1f6565c602f8aa901abf218a6e23d224f2d
Jan <57482512+jacka04@users.noreply.github.com>
2026-03-23 14:20:07 +0100

    Seguim amb UI, volum ui feta, falta brillo

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,23 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class PauseManager : MonoBehaviour
 {
     public GameObject pausePanel;
+    public GameObject settingsPanel;
+    public AudioMixer mainMixer;
+    public Image brightnessOverlay;
     private bool isPaused = false;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (isPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
+            if (isPaused) Resume();
+            else Pause();
         }
     }
 
@@ -31,18 +30,41 @@ public class PauseManager : MonoBehaviour
     public void Resume()
     {
         pausePanel.SetActive(false);
+        settingsPanel.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+    }
+
+    public void SetVolume(float value)
+    {
+        mainMixer.SetFloat("MasterVol", Mathf.Log10(value) * 20);
+    }
+
+    public void SetBrightness(float value)
+    {
+        if (brightnessOverlay != null)
+        {
+            Color c = brightnessOverlay.color;
+            c.a = 1f - value;
+            brightnessOverlay.color = c;
+        }
+    }
+
+    public void OpenSettings()
+    {
+        pausePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+        pausePanel.SetActive(true);
     }
 
     public void GoToMenu(string menuSceneName)
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(menuSceneName);
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
     }
 }
